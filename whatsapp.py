59b4981e946d592d0561ff2263444c0b16e08d46
collins kazee <collinskazee456@gmail.com>
2023-09-17 21:23:51 +0300

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -2,10 +2,10 @@ import random
 import pyautogui as pg
 import time 
 
-words =('world ', 'one and only ', 'angel','Princess', 'good girl')
+words =('idiot', 'donkey ', 'dumb','fool', 'bozo')
 time.sleep(8)
 
-for i in range (15):
+for i in range (25):
     a = random.choice(words)
     pg.write("i love you baby you are my  "+ a)
     pg.press ('enter')
